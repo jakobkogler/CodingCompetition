b3ae9de127be563eed7ef452f11b42d33e31184c
Jakob Kogler <jakob.kogler@gmail.com>
2015-11-15 17:30:43 +0100

    Added Binary Index Tree (Fenwick Tree)

---
+++ Algorithms/BIT.cpp
@@ -0,0 +1,71 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+// Binary Indexed Trees or Fenwick tree
+//
+// Update = O(log(n))
+// Sum = O(log(n))
+template<typename T>
+class BIT {
+    vector<T> v;
+
+public:
+    BIT(int n)
+    {
+        v = vector<T>(n + 1, 0);
+    }
+
+    // returns sum of the range [0...b-1]
+    T sum(int b)
+    {
+        T sum = 0;
+        for (b++; b; b -= (b & (-b)))
+        {
+            sum += v[b];
+        }
+        return sum;
+    }
+
+    // returns sum of the range [a...b]
+    T sum(int a, int b)
+    {
+        return sum(b) - (a ? sum(a - 1) : 0);
+    }
+
+    // update value of the k-th element by v (v can be +ve/inc or -ve/dec)
+    // i.e., increment or decrement kth element by a value v
+    void update(int idx, T value)
+    {
+        for (idx++; idx < v.size(); idx += (idx & (-idx)))
+        {
+            v[idx] += value;
+        }
+    }
+};
+
+int main()  {
+    BIT<int> b(10);
+    for (int i = 0; i < 10; i++)
+    {
+        b.update(i, i);
+    }
+
+    cout << "Test values:" << endl;
+    for (int i = 0; i < 10; i++)
+    {
+        cout << b.sum(i, i) << endl;
+    }
+    cout << endl;
+
+    cout << "Test sums from 0 to i:" << endl;
+    for (int i = 0; i < 10; i++)
+    {
+        cout << b.sum(i) << endl;
+    }
+    cout << endl;
+
+    cout << "Sum of first 5 elements: " << b.sum(5) << endl;
+    return 0;
+}
